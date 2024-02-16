ca4f6c4f39c9e8aaf15c89a39c4de897d173c4ef
neojarvis <techadmin@iamneo.ai>
2024-02-16 14:59:53 +0000

    Auto-save-commit-16/02/2024--20:29:51

--- Cricket1/dotnetapp/Data/ApplicationDbContext.cs
+++ Cricket1/dotnetapp/Data/ApplicationDbContext.cs
@@ -14,27 +14,26 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
  
        public DbSet<User> Users { get; set; }
         public DbSet<Account> Accounts { get; set; }
-        public DbSet<FDRequest> Players { get; set; }
-        public DbSet<Referee> Referees { get; set; }
-        public DbSet<Venue> Venues { get; set; }
-        public DbSet<Team> Teams { get; set; }
-        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<FDRequest> FDRequests { get; set; }
+        public DbSet<FixedDeposit> FixedDeposits { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Schedule>()
-                  .HasOne(s => s.Team1)
-                  .WithMany()
-                  .HasForeignKey(s => s.Team1Id)
-                  .OnDelete(DeleteBehavior.Restrict); // Use Restrict for one and NoAction for others
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     modelBuilder.Entity<Schedule>()
+        //           .HasOne(s => s.Team1)
+        //           .WithMany()
+        //           .HasForeignKey(s => s.Team1Id)
+        //           .OnDelete(DeleteBehavior.Restrict); // Use Restrict for one and NoAction for others
 
-            modelBuilder.Entity<Schedule>()
-                .HasOne(s => s.Team2)
-                .WithMany()
-                .HasForeignKey(s => s.Team2Id);    // Configure other relationships
+        //     modelBuilder.Entity<Schedule>()
+        //         .HasOne(s => s.Team2)
+        //         .WithMany()
+        //         .HasForeignKey(s => s.Team2Id);    // Configure other relationships
          
-            base.OnModelCreating(modelBuilder);
-        }
+        //     base.OnModelCreating(modelBuilder);
+        // }
 
 
 }
