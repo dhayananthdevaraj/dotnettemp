516f18dbc0da41336481b6d2f1c8bf28e0d512f0
neojarvis <techadmin@iamneo.ai>
2024-02-16 14:55:52 +0000

    Manual-save-commit-16/02/2024--20:25:49

--- Cricket1/dotnetapp/Data/ApplicationDbContext.cs
+++ Cricket1/dotnetapp/Data/ApplicationDbContext.cs
@@ -12,9 +12,9 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     }
 
  
-     public DbSet<User> Users { get; set; }
-        public DbSet<Event> Events { get; set; }
-        public DbSet<Player> Players { get; set; }
+       public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<FDRequest> Players { get; set; }
         public DbSet<Referee> Referees { get; set; }
         public DbSet<Venue> Venues { get; set; }
         public DbSet<Team> Teams { get; set; }
